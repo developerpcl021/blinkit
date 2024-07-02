a1335187cfaff38077bcfb28bc9dada9dc38c70f
nitinjain <nitinjain.work1991@gamil.com>
2024-07-02 17:40:19 +0530

    ass

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      borderColor: {
+        moreLighter: "#f2f2f2",
+      },
+      colors: {
+        "cart-green": "#0c831f",
+      },
+      boxShadow: {
+        "category-card": "rgba(0, 0, 0, 0.04) 2px 2px 8px",
+      },
+      fontSize: {
+        xsm: "12px",
+        md: "16px",
+      },
+    },
+  },
+};
